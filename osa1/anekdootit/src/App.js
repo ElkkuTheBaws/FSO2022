0991ae7ac71e392684b6dc6199bb919c0f4960d3
ElkkuTheBaws <58703912+ElkkuTheBaws@users.noreply.github.com>
2022-03-24 19:29:45 +0200

    Update App.js
    osa 1 done

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,21 +1,30 @@
 import { useState } from 'react'
 
+const points =  new Uint8Array(7)
+const copy = {...points}
+
 const Button = ({handleClick, text}) =>{
   return(
       <button onClick={handleClick}>{text}</button>
   )
 }
 
-const getRandomInt = (min, max) =>{
+const getRandomInt = (min, max, current) =>{
   min = Math.ceil(min);
   max = Math.floor(max);
+  let random =Math.floor(Math.random() * (max - min) + min)
 
-  return(
-      Math.floor(Math.random() * (max - min) + min)
-  )
+  for (let i = min; i < max; i++) {
+    //console.log(random, current)
+    if(random != current)
+      return random
+    else random =Math.floor(Math.random() * (max - min) + min)
+  }
 }
 
 const App = () => {
+
+
   const anecdotes = [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
@@ -25,21 +34,52 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-
   const [selected, setSelected] = useState(0)
+  const [hiehestVotes, setHighestVotes] = useState(0)
+  const [hiehestIndex, setHiehestIndex] = useState(0)
+  const [vote, setVoted] = useState(0)
 
+  const RandomAnecdote = () => setSelected(getRandomInt(0, anecdotes.length, selected))
+  const AddPoint = () =>{
+    copy[selected] += 1
+    setVoted(copy[selected])
 
+    if(copy[selected] > hiehestVotes){
+      setHiehestIndex(selected)
+      setHighestVotes(copy[selected])
+      console.log(hiehestVotes, copy[selected])
+    }
 
-  const RandomAnecdote = () => setSelected(getRandomInt(0, anecdotes.length))
+  }
 
   return (
       <div>
-        {anecdotes[selected]}
-        <div/>
+        <Anecdote ane = {anecdotes}
+                  sel = {selected}
+                  />
+
+        <Button handleClick={AddPoint}
+                text={'vote'}/>
+
         <Button handleClick={RandomAnecdote}
                 text={'next anecdote'}/>
+
+        <Anecdote ane = {anecdotes}
+                  sel = {hiehestIndex}
+        />
       </div>
   )
 }
+
+const Anecdote = (props) =>{
+  return(
+      <div>
+        <h1>Anecdote of the day</h1>
+        {props.ane[props.sel]}
+        <p>has {copy[props.sel]} votes</p>
+      </div>
+  )
+}
+
 
 export default App
